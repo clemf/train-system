332cfc1fef3da61fb639b61b2f0e0ed5d3f7d32d
Clem Freeman & Frank Meza <student@epicodus.com>
2015-01-21 10:44:14 -0800

    add and pass Station specs

--- lib/station.rb
+++ lib/station.rb
@@ -1,10 +1,25 @@
 class Station
-  define_method :initialize do |name|
+  attr_reader(:name)
+
+  define_method :initialize do |attributes|
+    @name = attributes.fetch(:name)
   end
+
   define_singleton_method :all do
+    stations = []
+    results = DB.exec("SELECT * FROM stations;")
+    results.each do |station|
+      name = station.fetch("name")
+      stations.push(Station.new({:name => name}))
+    end
+    stations
   end
+
   define_method :save do
+    DB.exec("INSERT INTO stations (name) VALUES ('#{@name}') RETURNING ID;")
   end
+
   define_method :== do |station|
+    name.==(station.name)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@ require 'rspec'
 require 'pg'
 require 'line'
 require 'station'
+
+DB = PG.connect({:dbname => 'train_system_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM stations *;")
+    DB.exec("DELETE FROM stops *;")
+    DB.exec("DELETE FROM lines *;")
+  end
+end
